b437184fbbf42861e9278823741055bedde2c46d
Kumar Harsh <harshkumar12799@gmail.com>
2022-10-06 11:07:22 +0530

    added lap functionality and clearlap

--- stopwatch/src/container/Stopwatch.js
+++ stopwatch/src/container/Stopwatch.js
@@ -3,6 +3,27 @@ import React from 'react'
 function Stopwatch() {
   const [time, setTime] = React.useState(0)
   const [timeon, setTimeon] = React.useState(false)
+  const [lapCount, setLapCount] = React.useState(1)
+
+  let hour = ("0" + Math.floor((time / 3600000) % 60)).slice(-2)
+  let minute = ("0" + Math.floor((time / 60000) % 60)).slice(-2)
+  let second = ("0" + Math.floor((time / 1000) % 60)).slice(-2)
+  let millisecond = ("0" + ((time / 10) % 100)).slice(-2)
+
+  function displayLap() {
+
+    setLapCount(prevLap => prevLap + 1)
+    
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(`${hour}:${minute}:${second}:${millisecond}`);
+    node.appendChild(textnode);
+    document.getElementById("display").appendChild(node);
+  }
+
+  function clearLap(){
+    setLapCount(1)
+    document.getElementById('display').innerHTML = '';
+  }
 
   React.useEffect(() => {
     let interval = null;
@@ -22,10 +43,10 @@ function Stopwatch() {
   return (
     <div>
       <div>
-        <span>{("0" + Math.floor((time / 3600000) % 60)).slice(-2)}:</span>
-        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
-        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
-        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+        <span>{hour}:</span>
+        <span>{minute}:</span>
+        <span>{second}:</span>
+        <span>{millisecond}</span>
       </div>
       <div>
         {!timeon && time === 0 &&
@@ -34,12 +55,24 @@ function Stopwatch() {
         {timeon &&
           (<button onClick={() => setTimeon(false)} >Stop</button>)
         }
+        {timeon &&
+          (<button onClick={displayLap} >Lap</button>)
+        }
         {!timeon && time !== 0 &&
           (<button onClick={() => setTimeon(true)} >Resume</button>)
         }
         {!timeon && time > 0 &&
           (<button onClick={() => setTime(0)} >Reset</button>)
         }
+        {!timeon && time > 0 && lapCount > 1 &&
+          (<button onClick={clearLap} >Clear Lap</button>)
+        }
+
+      </div>
+      <div>
+        <ol id='display'>
+
+        </ol>
       </div>
     </div>
   )
